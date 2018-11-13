8db6ea38ce8d9ccc4549bcaed10964ba7bd15d49
Alexander Piskurov <lolvalolee@gmail.com>
2018-11-13 09:59:58 +0200

    add chanel block

--- js/main.js
+++ js/main.js
@@ -1,11 +1,60 @@
+var chanelsList = {};
+var apiKey = 'eDBE76deU7L0H9mEBgxUKVR0VCnq0XBd'
+
+function createChatChanelContainer(chanel) {
+    var chatContainer= document.getElementById('chat-conatiner');
+
+    var container = document.createElement('div');
+    chatContainer.append(container);
+    container.className = 'chatBlock';
+
+    var chatTitle = document.createElement('div');
+    chatTitle.className = 'chatTitle';
+
+    var chatContent = document.createElement('div');
+    chatContent.className = 'chatContent';
+
+    var messageInput = document.createElement('textarea');
+    messageInput.className = 'chatMessageInput';
+
+    var sendMessageButton = document.createElement('button');
+    sendMessageButton.innerHTML = 'Send message';
+    container.append(chatTitle);
+    container.append(chatContent);
+    container.append(messageInput);
+    container.append(sendMessageButton);
+}
+
 function connectWebSocket() {
     var socket = new WebSocket('ws://vhost3.lnu.se:20080/socket/');
     socket.addEventListener('message', function(event) {
-        console.log('message receieve')
+        var message = JSON.parse(event.data);
+        if (chanelsList[event.channel]) {
+            chanelsList[event.channel].messages.push(
+                {
+                    'data': message.data,
+                    'username': message.username
+                }
+            )
+        }
     });
 
     socket.addEventListener('close', function(e) {
         console.log('connection closed');
     });
+
     return socket;
+}
+var connection = connectWebSocket();
+function connectChanel(event) {
+    var chanelName = document.getElementById('chanel-name').value;
+    chanelsList[chanelName] = {'messages': []};
+    createChatChanelContainer(chanelName);
+    connection.send(JSON.stringify({
+        "type": "message",
+        "data" : "message text",
+        "username": "Galya",
+        "channel": chanelName,
+        "key": apiKey
+    }));
 }
